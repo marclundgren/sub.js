3ed8ef0e73ee24dbda3721dcafff2fa47429fbc5
Marc Lundgren <marclundgren2.0@gmail.com>
2014-12-24 00:11:57 -0800

    sub.js

---
+++ sub.js
@@ -0,0 +1,15 @@
+// Sub utility
+
+/* jshint strict:false */
+var sub = function(str, obj) {
+  var keys = Object.keys(obj);
+
+  keys.map(function(key) {
+    var re = new RegExp('{' + key + '}', 'gm');
+    str = str.replace(re, obj[key]);
+  });
+
+  return str;
+};
+
+module.exports = sub;
